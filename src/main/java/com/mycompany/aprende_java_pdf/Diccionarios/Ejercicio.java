e5832deb0432de9780efe508b62c29cf694942e8
Jose Armando Diaz Segura <jdiazseg9611@universidadean.edu.co>
2020-06-03 14:33:43 -0500

    Se completa el primer Ejercicio( 6 en el libro) relacionado con la creacion de un HashMap, ademas de la comprobacion de existencia de la Llave y su correspondencia con el Value, para esto se crea un sencillo programa de comprobacion de usuario y contraseña con un limite de 3 intentos.

--- src/main/java/com/mycompany/aprende_java_pdf/Diccionarios/Ejercicio.java
+++ src/main/java/com/mycompany/aprende_java_pdf/Diccionarios/Ejercicio.java
@@ -14,7 +14,9 @@ public class Ejercicio {
      * una estructura de la clase HashMap.
      */
     public static void main(String[] args) {
+        //Creacion del hashMap
         HashMap<String, String> usuarios = new HashMap<>();
+        //Creacion de usuarios y contraseñas
         usuarios.put("user1", "15689");
         usuarios.put("user2", "isoadkas");
         usuarios.put("user3", "portelasd5454");
@@ -28,17 +30,17 @@ public class Ejercicio {
             user = scanner.next();
             System.out.println("Ingrese la contraseña: ");
             pass = scanner.next();
-            for (int i = 0; i < usuarios.size(); i++) {
-                if (usuarios.containsKey(user)) {
-                    if (usuarios.get(user).equals(pass)) {
-                        System.out.println("Ha accedido al area restringida");
-                        break;
-                    }
+            //Se Verifica si la llave(usuario) existe en el Hashmap
+            if (usuarios.containsKey(user)) {
+                //Se verifica si el valor(contraseña) corresponde a la llave
+                if (usuarios.get(user).equals(pass)) {
+                    System.out.println("Ha accedido al area restringida");
+                    break;
                 }
             }
             contador++;
-            break;
         }
+        //Si al completar los 3 intentos no se ha accedido al sistema se genera un mensaje y se termina el programa
         if (contador >= 3) {
             System.out.println("Lo siento, no tiene acceso al area restringida");
         }
