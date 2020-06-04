6e09c3d56600042f405941848233314a84b40e4f
Jose Armando Diaz Segura <jdiazseg9611@universidadean.edu.co>
2020-06-04 11:17:47 -0500

    Se observa el ejemplo de validacion y eliminacion de un archivo. Se prueba eliminando el archivo fruta.txt

---
+++ src/main/java/com/mycompany/aprende_java_pdf/FicherosTexto/Ejemplo05.java
@@ -0,0 +1,24 @@
+package com.mycompany.aprende_java_pdf.FicherosTexto;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Ejemplo05 {
+    /**
+     * Ejemplo del uso de la clase File
+     * Comprobacion de existencia y borrado de un fichero
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca el nombre del archivo que desea borrar: ");
+        String nombreFichero = scanner.nextLine();
+
+        File fichero = new File(nombreFichero);
+        if(fichero.exists()){
+            fichero.delete();
+            System.out.println("El fichero se ha borrado correctamente");
+        }else{
+            System.out.println("El fichero "+ nombreFichero + " no existe.");
+        }
+    }
+}
